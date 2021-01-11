945451deadfba250bf2be33f2d043c5c01dfe910
zhaned <zhanedmond@gmail.com>
2021-01-11 14:45:00 -0800

    added counter, first/last name, secret div template

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const displayResults = document.getElementById('display');
 
 // initialize state
 startButton.addEventListener('click', () =>{
+    let correctCounter = 0;
     alert('Hello! Lets begin.');
     const confirmAnswer = confirm('something');
     if (confirmAnswer === false){
         return;
     }
+    const firstName = prompt('First off, what\'s your first name?');
+    console.log(firstName);
+    const lastName = prompt('And your last name?');
+    console.log(lastName);
 
-
-
-
-
-
+    const completeString = `Hello, ${firstName} ${lastName}. You got ${correctCounter} out of 3 answers correct!`;
+    displayResults.textContent = completeString;
 });
 // set event listeners to update state and DOM

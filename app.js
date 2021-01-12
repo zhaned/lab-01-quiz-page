e8bd6a2c0bbcf712bade864b8998e6a7039d0059
zhaned <zhanedmond@gmail.com>
2021-01-11 18:20:30 -0800

    added css elements and some more car text

--- app.js
+++ app.js
@@ -2,24 +2,29 @@
 import { countAsAYes } from './utils.js';
 const startButton = document.getElementById('start-button');
 const displayResults = document.getElementById('display');
+const preReading = document.getElementById('text-info');
+
 
 // initialize state
 startButton.addEventListener('click', () =>{
     let correctCounter = 0;
 
     alert('Hello there!');
-    const confirmAnswer = confirm('Are you sure you want to begin?');
+    const confirmAnswer = confirm('Are you ready to begin?');
     if (confirmAnswer === false) return;
 
     const firstName = prompt('First off, what\'s your first name?');
     const lastName = prompt('And your last name?');
 
-    const firstAnswer = prompt('Was the first working vehicle made in 1672?');
+    const firstAnswer = prompt(`Alright, ${firstName} ${lastName}. Lets begin. Was the first working vehicle made in 1672? (Yes/No)`);
     if (countAsAYes(firstAnswer)) ++correctCounter;
-    const secondAnswer = prompt('Can your bike be considered a car?');
+    const secondAnswer = prompt(`Can your bike be considered a car?`);
     if (!countAsAYes(secondAnswer)) ++correctCounter;
-    const thirdAnswer = prompt('Was Henry Ford the first person to make large scale production of affordable cars?');
+    const thirdAnswer = prompt(`Was Henry Ford the first person to make large scale production of affordable cars? (Yes/No)`);
     if (!countAsAYes(thirdAnswer)) ++correctCounter;
+
+    alert(`Thank you! Your results are posted below the submit button.`);
+    preReading.style.display = 'none';
 
     const completeString = `Hello, ${firstName} ${lastName}. You got ${correctCounter} out of 3 answers correct!`;
     displayResults.textContent = completeString;
